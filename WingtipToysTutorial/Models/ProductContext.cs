db420fec72aed0b98e700608ec59109af2291b33
lucachecha <82596990+lucachecha@users.noreply.github.com>
2021-05-05 16:54:06 -0300

    Checkout

--- WingtipToysTutorial/Models/ProductContext.cs
+++ WingtipToysTutorial/Models/ProductContext.cs
@@ -10,5 +10,7 @@ namespace WingtipToys.Models
         public DbSet<Product> Products { get; set; }
 
         public DbSet<CartItem> ShoppingCartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
     }
 }
