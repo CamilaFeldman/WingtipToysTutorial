bfbeee22d7f43dc50fb20f9d44dd6f22faaa15a0
ItsGian <gianimarco2002@gmail.com>
2021-05-11 16:13:20 -0300

    Error Soto
    Errores

---
+++ WingtipToysTutorial/App_Start/StartupAuth.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(WingtipToysTutorial.App_Start.StartupAuth))]
+
+namespace WingtipToysTutorial.App_Start
+{
+    public class StartupAuth
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Para obtener más información sobre cómo configurar la aplicación, visite https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
